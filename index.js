ddb4e700a8855017e0ee75522ad43774b55180df
Jidraf Kimachia <raphjid170@gmail.com>
2022-06-17 10:17:30 +0300

    phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-function scuberGreetingForFeet (feet){
-if (feet <= 400) {
+function scuberGreetingForFeet (ride){
+if (ride < 400) {
 return 'This one is on me!';
-} else if (1999 < feet && feet < 2500)
-return "I will gladly take your thirty bucks.";
-
-  return "No can do.";
+} 
+else if (ride > 2000 && ride <= 2500){
+  return "I will gladly take your thirty bucks."
 }
-
+  else if(ride > 2500){
+    return  'No can do.'
+  }
+}  
 
 function ternaryCheckCity (city) {
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  let message;
+  city === "NYC" ? message = 'Ok, sounds good.' : message = 'No go.'
+  return message
 }
 
 function switchOnCharmFromTip (tip) {
+  let mess;
   switch (tip) {
     case 'generous':
-      return 'Thank you so much.';
+      mess = 'Thank you so much.';
+      break;
       case 'not as generous':
-        return 'Thank you.';
-        default:
-          return 'Bye.';
+        mess =  'Thank you.';
+        break;
+      default:
+        mess = "Bye.";
+        break;
   }
+  return mess
 }
